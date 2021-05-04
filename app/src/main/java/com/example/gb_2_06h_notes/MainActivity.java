649a678c48c1cd42d18b9f245928bc8897ecb886
gravitana <v.a.grahov@ya.ru>
2021-05-04 17:56:08 +0500

    Toolbar

--- app/src/main/java/com/example/gb_2_06h_notes/MainActivity.java
+++ app/src/main/java/com/example/gb_2_06h_notes/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.gb_2_06h_notes;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -16,6 +19,32 @@ public class MainActivity extends AppCompatActivity implements NotesListFragment
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        toolbar.setTitle(R.string.app_title);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.action_new) {
+                    Toast.makeText(MainActivity.this, R.string.action_new, Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+                if (item.getItemId() == R.id.action_sort) {
+                    Toast.makeText(MainActivity.this, R.string.action_sort, Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+                if (item.getItemId() == R.id.action_settings) {
+                    Toast.makeText(MainActivity.this, R.string.action_settings, Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+                return false;
+            }
+        });
 
         isLandscape = getResources().getBoolean(R.bool.isLandscape);
 
